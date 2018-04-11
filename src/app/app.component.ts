93904d4ae1310e6dd9fb979aab75c4fe06bb5904
Juan Mateos Silva <sofarojo@gmail.com>
2018-04-11 20:07:10 +0200

    Added ErrorPage. Created DataService. When database is loaded the app will load the home page otherwise it will load the error page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,50 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { DataService } from '../services/data';
 import { HomePage } from '../pages/home/home';
+import { ErrorPage } from '../pages/error/error';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private dataService: DataService, private sqlite: SQLite) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.openDatabase()
+      .then(()=> {
+        this.rootPage = HomePage;
+      })
+      .catch(() => {
+        this.rootPage = ErrorPage;
+      })
+    });
+  }
+
+  openDatabase(): Promise<boolean> {
+    const dbConfig:any = {
+      name: 'database.db',
+      location: 'default',
+      createFromLocation: 1
+    };
+
+    return new Promise((resolve, reject) => {
+      this.sqlite.create(dbConfig)
+        .then((db: SQLiteObject) => {
+          this.dataService.setDatabase(db);
+          resolve(true);
+        })
+        .catch(e => {
+          reject();
+        });
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ErrorPage } from '../pages/error/error';
+
+import { SQLite } from '@ionic-native/sqlite';
+import { DataService } from '../services/data';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ErrorPage
   ],
   imports: [
     BrowserModule,
@@ -19,11 +24,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ErrorPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    SQLite,
+    DataService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
