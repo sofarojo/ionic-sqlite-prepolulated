318854b29195d4850e8fe9dd3deb776993551263
Juan Mateos Silva <sofarojo@gmail.com>
2018-04-13 19:14:14 +0200

    Added model. Read data from database and displayed in a list. Package cordova-sqlite-storage replaced by cordova-sqlite-ext in order to access to prepopulated tables. Some clean code and refactoring

--- src/services/data.ts
+++ src/services/data.ts
@@ -1,4 +1,5 @@
 import { SQLiteObject } from '@ionic-native/sqlite';
+import { Word } from '../models/word';
 
 export class DataService {
   db: SQLiteObject = null;
@@ -8,4 +9,33 @@ export class DataService {
   setDatabase(db: SQLiteObject) {
     this.db = db;
   }
+
+  getWordList(): Promise<Array<Word>> {
+    return new Promise((resolve, reject) => {
+
+      if (this.db === null || this.db === undefined) {
+        resolve([]);
+      }
+
+      this.db.executeSql('SELECT wordid as wordId, front, back, additional FROM words',
+       []).then((response) => {
+        const words: Array<Word> = this.mapWords(response);
+        resolve(words);
+      }).catch(e => reject(e));
+    })
+  };
+
+  private mapWords(response: any): Array<Word> {
+    let words:Array<Word> = new Array<Word>();
+
+    for (let index = 0; index < response.rows.length; index++) {
+      words.push( this.mapWord(response.rows.item(index)) );
+    }
+
+    return words;
+  }
+
+  private mapWord(item: any): Word {
+    return Object.assign(new Word(), item);
+  }  
 }
